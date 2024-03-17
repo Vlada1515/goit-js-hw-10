de7453545c2db68ddc1c48681059e556e8a3c04b
Vlada1515 <vladavalianova5@gmail.com>
2024-03-17 17:23:44 +0100

    update

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,7 +1,6 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
-import iziToast from 'izitoast';
-import 'izitoast/dist/css/iziToast.min.css';
+
 
 const inputDateTimePicker = document.getElementById('datetime-picker');
 const btnStart = document.querySelector('[data-start]');
