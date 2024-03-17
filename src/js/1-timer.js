00446089c4164cdb7cc26effe67e1c4d02d12a09
Vlada1515 <vladavalianova5@gmail.com>
2024-03-17 17:37:52 +0100

    update

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,5 +1,9 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
+// Описаний у документації
+import iziToast from "izitoast";
+// Додатковий імпорт стилів
+import "izitoast/dist/css/iziToast.min.css";
 
 
 const inputDateTimePicker = document.getElementById('datetime-picker');
